cdae7c9b1662aafdebab8c7ab64ba6d5ce886414
AgungPrabowo <agungprabowo112@gmail.com>
2018-04-11 19:20:57 +0700

    Learn React Native

---
+++ index.js
@@ -0,0 +1,16 @@
+// Import Library
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+// create Component
+const App = () => (
+        <View>
+            <Header headerText={'Albums'} />
+            <AlbumList />
+        </View>
+    );
+
+// Render to device
+AppRegistry.registerComponent('albums', () => App);
